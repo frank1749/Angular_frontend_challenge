500c5c00bb071feea4cc5de927cea828d17a475e
Frank Murillo <frank1749@hotmail.com>
2024-01-07 13:28:45 -0500

    Avance 1service

--- src/app/constructor/registro-proyecto/registro-proyecto.component.ts
+++ src/app/constructor/registro-proyecto/registro-proyecto.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ApiService } from '../../services/api.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-registro-proyecto',
@@ -11,29 +12,58 @@ export class RegistroProyectoComponent implements OnInit {
 
   form!: FormGroup;
 
-  constructor(private fb: FormBuilder, private apiService: ApiService) { }
+  constructor(private formBuilder: FormBuilder, private apiService: ApiService) { }
 
   ngOnInit(): void {
     this.initForm();
   }
 
   initForm(): void{
-    this.form = this.fb.group({
+    this.form = this.formBuilder.group({
       nombreProyecto: ['', Validators.required],
       fechaInicial: ['', Validators.required],
-      fechaFinal: ['', Validators.required]
+      fechaFinal: ['', Validators.required],
+      items: this.formBuilder.array([], Validators.required)
     });
   }
 
+  // Getter conveniente para acceder a los controles del FormArray de ítems
+  get items(): FormArray {
+    return this.form.get('items') as FormArray;
+  }
+
+  // Método para agregar un nuevo ítem al FormArray
+  addItem() {
+    const item = this.formBuilder.group({
+      item: '',
+      valorUnitarioActual: ''
+    });
+
+    this.items.push(item);
+  }
+  // Método para eliminar un ítem del FormArray
+  removeItem(index: number) {
+    this.items.removeAt(index);
+  }
+
   onFormSubmit(): void {
-    console.log('Name:', this.form.value);
+    console.log('form values:', this.form.value);
     this.apiService.createProject(this.form.value).subscribe(
       res => {
-        alert('Registro creado');
+        Swal.fire({
+          title: "Buen trabajo!",
+          text: "Proyecto creado exitosamente!",
+          icon: "success"
+        });
         this.initForm();
       },
       err =>{
         console.log(err);
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: err.error.msg
+        });
       }
     );
   }
